e739a8fc5d44fa1a8b84d8389bd1ae63f5c3c52a
5684185+vsariola@users.noreply.github.com <5684185+vsariola@users.noreply.github.com>
2023-01-04 21:17:17 +0200

    Change to 21e3 time and optimize size by moving envelope around.

--- etude-de-la-synthese-additive.js
+++ etude-de-la-synthese-additive.js
@@ -1,7 +1,7 @@
 (_ => {
   if (!t) // if t == 0, initialize delay ring buffer into empty value
     d = [];
-  p = 2e4; // time per beat
+  p = 21e3; // time per beat
   // x = left channel, y = right channel
   x = y = d[t % p] / 3 || 0; // pull values from the delay buffer. could be undefined, so we default to 0
   u = t / p / 2; // time in beats
@@ -11,12 +11,11 @@
       r = u * 2 ** (j - 5) * k; // this is the "row" in the pattern, different channels loop at different durations
       v = r % 1; // position within the row
       z = u < 144 && // this is not too important, but wanted a clean ending for the song
-        3 ** -(v * i * k ** .5 + .003 / v) / 8 * // envelope, higher partials decay faster
         sin(
           PI * 3 * (2846666506 >> (r & 1 + u / 16 & 7) * 4 & 15) * // frequency from min7 chord (2846666506)
           [6, 6, 8, 9, 6][u & u / 32] * // i-iv-v chord progression
           i ** k * u // k=0: partials 1,2,3... k=1: partials 1,4,9... k=2: partials 1,8,21...
-        );
+        ) / 3 ** (v * i * k ** .5 + .003 / v) / 8; // envelope, higher partials decay faster;
       d[t % p] = x += sin(n = i++ * u / 99) * z; // evolving weights for partials, save left chn to buffer
       y += sin(n * i) * z; // right channel partials are given different weights
     }
