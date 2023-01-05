68693d31c69e3188883f7b56187e207dc9abb971
5684185+vsariola@users.noreply.github.com <5684185+vsariola@users.noreply.github.com>
2023-01-05 16:42:05 +0200

    Optimize size.

--- etude-de-la-synthese-additive.js
+++ etude-de-la-synthese-additive.js
@@ -1,10 +1,9 @@
 (_ => {
   if (!t) // if t == 0, initialize delay ring buffer into empty value
     d = [];
-  p = 21e3; // time per beat
   // x = left channel, y = right channel
-  x = y = d[t % p] / 3 || 0; // pull values from the delay buffer. could be undefined, so we default to 0
-  u = t / p / 2; // time in beats
+  x = y = d[m = t % 21e3] / 3 || 0; // pull values from the delay buffer. could be undefined, so we default to 0
+  u = t / 42e3; // time in beats
   for (j = 0; ++j < 7;) { // j goes from 1 to 6, this is the "channel"
     for (i = 1; i < 7;) { // i goes from 1 to 6, this the partial
       k = j % 3 + 1; // this is the instrument, every two channels share the same instrument
@@ -16,7 +15,7 @@
           [6, 6, 8, 9, 6][u & u / 32] * // i-iv-v chord progression
           i ** k * u // k=0: partials 1,2,3... k=1: partials 1,4,9... k=2: partials 1,8,21...
         ) / 3 ** (v * i * k ** .5 + .003 / v) / 9; // envelope, higher partials decay faster;
-      d[t % p] = x += sin(n = i++ * u / 99) * z; // evolving weights for partials, save left chn to buffer
+      d[m] = x += sin(n = i++ * u / 99) * z; // evolving weights for partials, save left chn to buffer
       y += sin(n * i) * z; // right channel partials are given different weights
     }
   }
